04d510dac9ce0e1a58ff1436255aa33f6bfa9688
Igor <imitroshichev@gmail.com>
2022-06-05 14:52:40 +0300

    first commit

--- main.js
+++ main.js
@@ -2,72 +2,33 @@ const cat = document.getElementById('cat');
 const cont = document.querySelector('.conteiner');
 
 cat.style.left = cont.clientWidth / 2 + "px";
-let j = 0;
+
+let timerId = 0;
+let indent = 1;
 
 cat.addEventListener('mouseenter', e => {
     
-    if (cat.style.left === '' || cat.style.left === cont.clientWidth / 2 + "px") {
-      let timerId = setInterval(run, 250);
-      
-    return;
-    }
-
-    if (cat.style.left !== '') {
-      cat.style.left = '';
-      setInterval(run2, 250);
-    }
+  if (timerId) {
+    clearInterval(timerId);
+    indent = -indent;
+  }
+  timerId = setInterval(run, 10);
     
 });
 
 function run() {
-
-
   if (parseInt(cat.style.left) >= cont.clientWidth - cat.clientWidth) {
-    console.log(666);
-    return;
+    indent = -indent;
   }
+
+  if (parseInt(cat.style.left) <= 0) {
+    indent = -indent;
+  }
+
   let i = parseInt(cat.style.left);
  
   if (isNaN(i)) {
     i = 0;
   }
-  cat.style.left = i + 5 + "px";
+  cat.style.left = i + indent + "px";
 }
-
-function run2() {
-  if (parseInt(cat.style.left) >= cont.clientWidth - cat.clientWidth) {
-    console.log(cat.style.left);
-    console.log(cont.clientWidth - cat.clientWidth + "px");
-    console.log('run1');
-    return;
-  }
-  let i = parseInt(cat.style.left);
-  console.log(i);
-  if (isNaN(i)) {
-    i = 0;
-  }
-  cat.style.left = i - 5 + "px";
-    
-}
-
-
-function run1() {
-  if (cat.style.left >= cont.clientWidth - cat.clientWidth + "px") {
-    console.log('run2');
-    return;
-  }
- 
-  let i = cat.style.left;
-  cat.style.left = i + 5 + "px";
-  
-}
-
-/*var intervalID = setInterval(myCallback, 500, 'Parameter 1', 'Parameter 2');
-
-function myCallback(a, b)
-{
- // Your code here
- // Parameters are purely optional.
- console.log(a);
- console.log(b);
-} */
